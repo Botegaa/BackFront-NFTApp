47c6c98175f9949e95f5b8ea506cbd79bb81a768
botegaa <botega321@gmail.com>
2024-08-05 17:49:20 -0300

    Initial Commit

---
+++ NFTApp/LoginScreen.swift
@@ -0,0 +1,128 @@
+//
+//  LoginScreen.swift
+//  NFTApp
+//
+//  Created by Pedro Botega on 05/08/24.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    lazy var subImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "BGLogin")
+        return image
+    }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "BFLogin")
+        return image
+    }()
+    
+    lazy var loginLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "BF NFT"
+        return label
+    }()
+    
+    lazy var descriptionLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "O marketplace de NFTs da BackFront Academy"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var loginTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.autocorrectionType = .no
+        tf.backgroundColor = UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .emailAddress
+        tf.attributedPlaceholder = NSAttributedString(string: "Login", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white.withAlphaComponent(0.4)] )
+        tf.textColor = .white
+        tf.clipsToBounds = true
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = UIColor.white.cgColor
+        return tf
+    }()
+    
+    lazy var passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.autocorrectionType = .no
+        tf.backgroundColor = UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .default
+        tf.isSecureTextEntry = true
+        tf.attributedPlaceholder = NSAttributedString(string: "Senha", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white.withAlphaComponent(0.4)] )
+        tf.textColor = .white
+        tf.clipsToBounds = true
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = UIColor.white.cgColor
+        return tf
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addElements()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addElements(){
+        addSubview(subImageView)
+        addSubview(logoAppImageView)
+        addSubview(loginLabel)
+        addSubview(descriptionLabel)
+        addSubview(loginTextField)
+        addSubview(passwordTextField)
+    }
+    
+    private func configConstraints (){
+        NSLayoutConstraint.activate([
+            subImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            subImageView.topAnchor.constraint(equalTo: topAnchor),
+            subImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            subImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+         
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 55),
+            logoAppImageView.heightAnchor.constraint(equalToConstant: 108),
+            logoAppImageView.widthAnchor.constraint(equalToConstant: 108),
+            logoAppImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            loginLabel.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 16),
+            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 4),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+
+        
+            loginTextField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 32),
+            loginTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            loginTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            loginTextField.heightAnchor.constraint(equalToConstant: 39),
+            
+            passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 11),
+            passwordTextField.leadingAnchor.constraint(equalTo: loginTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: loginTextField.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalTo: loginTextField.heightAnchor),
+        ])
+    }
+}
---
+++ NFTApp/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NFTApp
+//
+//  Created by Pedro Botega on 05/08/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    var loginScreen : LoginScreen?
+
+    
+    override func loadView() {
+        loginScreen = LoginScreen()
+        view = loginScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
